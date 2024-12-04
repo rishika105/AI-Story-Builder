926394286ef5e9d75d0b9efa12b826c2666c9a93
Rishika Agarwal <rishikaagarwal2316@gmail.com>
2024-12-04 23:58:43 +0530

    styling frontend

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      HII
-    </div>
-  )
-}
+    <div className="bg-darkgray-800 w-full h-full">
+      <div className="flex flex-col justify-center items-center">
+        <div>
+          <div className="text-darkgray-200 font-bold text-2xl p-6">
+            AI STORY BUILDER
+          </div>
+          <p className="text-darkgray-50">
+            Unleash Your Imagination - Build Stories One Sentence at a Time
+          </p>
 
-export default Home
+          <div className="text-white p-4 ">
+            Welcome to our story generation platform, where creativity meets
+            technology! Whether you're a budding writer, an avid storyteller, or
+            trying to engage yourself in like a game or just someone looking to
+            explore the magic of words, this is the perfect place for you. Our
+            AI-powered system helps you craft engaging and imaginative stories,
+            sentence by sentence, while keeping the essence of your narrative
+            intact. Pick a genre, start with a single idea, and watch your story
+            unfold in ways you never imagined. Let’s make storytelling
+            interactive, fun, and limitless! Also You can copy the entire story
+            prompt to let others read or post somewhere!
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
