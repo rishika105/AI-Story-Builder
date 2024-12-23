d5690ad5cc41faeac14ce56577673b15b662df41
Rishika Agarwal <rishikaagarwal2316@gmail.com>
2024-12-23 21:40:55 +0530

    updated

--- src/pages/AIStoryChatBot.jsx
+++ src/pages/AIStoryChatBot.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 
 const AIStoryChatBot = () => {
   return (
     <>
       <Navbar />
-      <div className="bg-deepblue-800 overflow-auto overflow-y h-[490px] w-[70%] flex relative justify-center items-center mx-auto"></div>
-      <div className="bg-deepblue-800 relative">
+      <div className="bg-deepblue-600 flex relative justify-center items-center mx-auto w-[70%] h-[90vh]">
+        {/* chat section */}
+       <div className=" overflow-y overflow-auto h-[40%]">
+
+       </div>
+      
         {/* send message */}
         <input
           className="bg-darkgray-400 bg-opacity-20 h-[65px] w-[70%] rounded-md bottom-0 flex absolute pl-4 text-white focus:outline-none mb-[-300px]"
           placeholder="Send message"
         ></input>
+    
       </div>
+   
     </>
   );
 };
